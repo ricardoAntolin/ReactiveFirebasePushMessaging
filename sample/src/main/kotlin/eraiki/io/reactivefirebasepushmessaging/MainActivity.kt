38912da0508c339b6bbe261a470d075ef6fdc84b
Ricardo Antolin <richarantolin@gmail.com>
2017-06-24 11:59:42 +0200

    example coded

--- sample/src/main/kotlin/eraiki/io/reactivefirebasepushmessaging/MainActivity.kt
+++ sample/src/main/kotlin/eraiki/io/reactivefirebasepushmessaging/MainActivity.kt
@@ -1,12 +1,60 @@
 package eraiki.io.reactivefirebasepushmessaging
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.firebase.messaging.RemoteMessage
+import eraiki.io.rxpush.usecases.OnMessageReceivedUseCase
+import eraiki.io.rxpush.usecases.RefreshTokenUseCase
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableObserver
 
 class MainActivity : AppCompatActivity() {
+    val threadExecutor = JobExecutor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        subscribeToFCMRegistration()
+        subscribeToNotifications()
+    }
+
+    fun subscribeToNotifications(){
+        OnMessageReceivedUseCase(threadExecutor, AndroidSchedulers.mainThread())
+                .execute(NotificationObserver())
+    }
+
+    fun subscribeToFCMRegistration(){
+        RefreshTokenUseCase(threadExecutor, AndroidSchedulers.mainThread())
+                .execute(FCMTokenReceiverObserver())
+    }
+
+    fun enrollDevice(deviceToken:String){
+        //Send deviceToken to own message server
+    }
+
+    inner class NotificationObserver: DisposableObserver<RemoteMessage>() {
+        override fun onNext(t: RemoteMessage) {
+            PushNotification.buildAndShowNotification(this@MainActivity,t)
+        }
+
+        override fun onComplete() {
+        }
+
+        override fun onError(exception: Throwable) {
+
+        }
+    }
+
+    inner class FCMTokenReceiverObserver: DisposableObserver<String>() {
+        override fun onNext(t: String) {
+            enrollDevice(t)
+        }
+
+        override fun onComplete() {
+        }
+
+        override fun onError(exception: Throwable) {
+
+        }
     }
 }
